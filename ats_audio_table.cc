bf187af0a3cd37d1747845e4dd5ce8aa5d950160
MinorLegato <anton.siderud@gmail.com>
2021-04-06 17:35:35 +0200

    gfd

--- ats_audio_table.cc
+++ ats_audio_table.cc
@@ -14,7 +14,8 @@
 #define AUDIO_PATH "assets/sounds/"
 #endif
 
-struct Audio_Entry {
+struct AudioEntry
+{
     b32 not_empty;
     
     cs_loaded_sound_t loaded;
@@ -23,15 +24,17 @@ struct Audio_Entry {
     char name[64];
 };
 
-static struct {
+static struct
+{
     cs_context_t* context;
     cs_plugin_interface_t pitch_plugin;
     cs_plugin_id_t pitch_id;
 } audio;
 
-static Audio_Entry audio_table[AUDIO_TABLE_SIZE];
+static AudioEntry audio_table[AUDIO_TABLE_SIZE];
 
-static void audio_init(void) {
+static void InitAudio(void)
+{
     audio.context = cs_make_context(platform.native, 44100, 16 * 4096, 1024, NULL);
     
     cs_spawn_mix_thread(audio.context);
@@ -41,29 +44,31 @@ static void audio_init(void) {
     audio.pitch_id      = cs_add_plugin(audio.context, &audio.pitch_plugin);
 }
 
-#define get_audio(name) get_audio_internal(name, AUDIO_PATH name ".wav")
+#define GetAudio(name) GetAudioInternal(name, AUDIO_PATH name ".wav")
 
-struct Audio_ID {
+struct AudioID
+{
     u16 index;
 };
 
-static Audio_ID get_audio_internal(const char* name, const char* path) {
-    u32 hash    = hash_cstr(name);
+static AudioID GetAudioInternal(const char* name, const char* path)
+{
+    u32 hash    = HashCStr(name);
     u16 index   = hash & (AUDIO_TABLE_SIZE - 1);
     
     if (index == 0) index++;
     
-    while (audio_table[index].not_empty) {
-        if (strcmp(audio_table[index].name, name) == 0) {
+    while (audio_table[index].not_empty)
+    {
+        if (strcmp(audio_table[index].name, name) == 0)
             return { .index = index };
-        }
         
         index = (index + 1) & (AUDIO_TABLE_SIZE - 1);
         
         if (index == 0) index++;
     }
     
-    Audio_Entry* entry = &audio_table[index];
+    AudioEntry* entry = &audio_table[index];
     
     entry->not_empty = true;
     strcpy(entry->name, name);
@@ -71,15 +76,17 @@ static Audio_ID get_audio_internal(const char* name, const char* path) {
     entry->loaded  = cs_load_wav(path);
     entry->playing = cs_make_def(&entry->loaded);
     
-    return { .index = index };
+    return { index };
 }
 
-static void pause_audio(b32 pause) {
+static void PauseAudio(b32 pause)
+{
     cs_lock(audio.context);
     
     cs_playing_sound_t* playing = cs_get_playing(audio.context);
     
-    while (playing) {
+    while (playing)
+    {
         cs_pause_sound(playing, pause);
         playing = playing->next;
     }
@@ -87,17 +94,22 @@ static void pause_audio(b32 pause) {
     cs_unlock(audio.context);
 }
 
-static void kill_all_audio(void) {
+static void KillAllAudio(void)
+{
     cs_stop_all_sounds(audio.context);
 }
 
-static Audio_Entry* audio_get_entry(Audio_ID id) {
+static AudioEntry* GetAudioEntry(AudioID id)
+{
     if (!id.index || id.index > AUDIO_TABLE_SIZE) return NULL;
+
     return audio_table[id.index].not_empty? &audio_table[id.index] : NULL;
 }
 
-static void play_audio(Audio_ID id, f32 volume = 1.0) {
-    if (Audio_Entry* entry = audio_get_entry(id)) {
+static void PlayAudio(AudioID id, f32 volume = 1.0)
+{
+    if (AudioEntry* entry = GetAudioEntry(id))
+    {
         cs_playing_sound_t* playing = cs_play_sound(audio.context, entry->playing);
         
         if (!playing) return;
@@ -108,14 +120,19 @@ static void play_audio(Audio_ID id, f32 volume = 1.0) {
     }
 }
 
-static cs_playing_sound_t* play_looped_audio(Audio_ID id, f32 volume = 1.0) {
-    if (Audio_Entry* entry = audio_get_entry(id)) {
+static cs_playing_sound_t* PlayLoopedAudio(AudioID id, f32 volume = 1.0)
+{
+    if (AudioEntry* entry = GetAudioEntry(id))
+    {
         cs_playing_sound_t* playing = cs_play_sound(audio.context, entry->playing);
         
-        if (!playing) { return NULL; }
+        if (!playing)
+            return NULL;
         
         cs_lock(audio.context);
+
         playing->looped = 1;
+
         cs_set_volume(playing, volume, volume);
         cs_unlock(audio.context);
         
@@ -125,47 +142,52 @@ static cs_playing_sound_t* play_looped_audio(Audio_ID id, f32 volume = 1.0) {
     return NULL;
 }
 
-static void play_music(Audio_ID id, f32 volume = 1.0) {
+static void PlayMusic(AudioID id, f32 volume = 1.0)
+{
     static cs_playing_sound_t* playing = NULL;
     
-    if (playing && cs_is_active(playing)) {
+    if (playing && cs_is_active(playing))
         cs_stop_sound(playing);
-    }
     
-    if (Audio_Entry* entry = audio_get_entry(id)) {
+    if (AudioEntry* entry = GetAudioEntry(id))
+    {
         playing = cs_play_sound(audio.context, entry->playing);
         
-        if (!playing) { return; }
+        if (!playing) return;
         
         cs_lock(audio.context);
+
         playing->looped = true;
+
         cs_set_volume(playing, volume, volume);
         cs_unlock(audio.context);
     }
 }
 
-static void play_audio_from_source(Audio_ID id, v3 pos, v3 dir, v3 source, f32 volume = 1.0, f32 max_distance = 16.0) {
-    f32 sound_distance  = dist(pos, source);
+static void PlayAudioFromSource(AudioID id, v3 pos, v3 dir, v3 source, f32 volume = 1.0, f32 max_distance = 16.0)
+{
+    f32 sound_distance  = Dist(pos, source);
     f32 final_volume    = volume * max(1 - sound_distance / max_distance, 0);
 
     if (final_volume <= 0) return;
 
-    if (Audio_Entry* entry = audio_get_entry(id)) {
-        f32 pan = get_angle(dir.xy, norm(source.xy - pos.xy)) / PI;
+    if (AudioEntry* entry = GetAudioEntry(id))
+    {
+        f32 pan = GetAngle(dir.xy, Norm(source.xy - pos.xy)) / PI;
 
-        if(pan > 0.5f) pan = 1.0f - pan;
-        if(pan <-0.5f) pan =-1.0f - pan;
+        if (pan > 0.5f) pan = 1.0f - pan;
+        if (pan <-0.5f) pan =-1.0f - pan;
 
         pan += 0.5f;
         pan = 1.0f - pan;
         
         cs_playing_sound_t* playing = cs_play_sound(audio.context, entry->playing);
         
-        if (!playing) { return; }
+        if (!playing) return;
         
         cs_lock(audio.context);
         
-        csp_set_pitch(playing, 1.0f + rand_f32(-0.05f, 0.1f), audio.pitch_id);
+        csp_set_pitch(playing, 1.0f + RandF32(-0.05f, 0.1f), audio.pitch_id);
         cs_set_pan(playing, pan);
         
         cs_set_volume(playing, final_volume, final_volume);
