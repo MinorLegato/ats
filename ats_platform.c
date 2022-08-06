e1a7722a54f01f07b93daee5b5c3a41bf1aee951
MinorLegato <anton.siderud@gmail.com>
2022-08-06 12:08:27 +0200

    asdf

--- ats_platform.c
+++ ats_platform.c
@@ -1,6 +1,6 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-#pragma comment(lib, "../ats/lib/glfw3_mt.lib")
+#pragma comment(lib, "../../ats/lib/glfw3_mt.lib")
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "gdi32.lib")
 #pragma comment(lib, "shell32.lib")
